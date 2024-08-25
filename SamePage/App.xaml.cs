0971d78791da895c542a400cac90de7fcc00bd3c
Justine Sico <justinesico2243@gmail.com>
2024-08-25 13:00:04 +0800

    Setting up

---
+++ SamePage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SamePage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SamePage/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SamePage
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
